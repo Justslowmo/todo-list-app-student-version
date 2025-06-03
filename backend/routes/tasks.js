db59afc2f92c7f9628f37abd37acb498c659d8e6
Justslowmo <bryan.paulino@bronxsoftware.org>
2025-06-03 00:23:57 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,18 +2,18 @@ const express = require("express");
 const router = express.Router();
 // There is a bug in line 4 you need to fix it
 const taskModel = require("../model/taskModel");
-
-//Write a comment describing the purpose of this route
+const taskModel = new TaskModel();
+//This route retrieves and returns a list of all tasks from the database
 router.get("/", async (req, res) => {
   const tasks = await taskModel.getTasks();
   res.json(tasks);
 });
 
-// Write a comment describing the purpose of this route
+// This route receives task data from the client and creates a new task in the database
 router.post("/", async (req, res) => {
   //there is a bug in line 15 you need to fix
   const { name, description } = req.body;
-  const task = await taskModel.addTask(title, description);
+  const task = await taskModel.addTask(name, description); // changed title to name i think that was the only way to fix line 15 code
   res.status(201).json(task);
 });
 
